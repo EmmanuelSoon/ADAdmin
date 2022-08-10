c779d1c6fad28076a181282f5ff5e9475257afd9
Emmanuel Soon <reach.emmanuelsoon@gmail.com>
2022-08-10 12:11:32 +0800

    users use case

--- src/components/MainComponent.jsx
+++ src/components/MainComponent.jsx
@@ -3,7 +3,8 @@ import {Routes , Route, Navigate } from 'react-router-dom';
 
 import WrongPredict from './WrongPredictComponent';
 import Report from './ReportComponent';
-
+import Home from './HomeComponent';
+import User from './UserComponent';
 
 function Main () {
 
@@ -12,8 +13,10 @@ function Main () {
             <Routes>
                     <Route path="/wrongpredict" element={<WrongPredict />}/>
                     <Route path="/reports" element={<Report />}/>
+                    <Route path="/" element={<Home />}/>
+                    <Route path="/users" element={<User />}/>
 
-                    <Route path="*" element= {<Navigate to="/wrongpredict" replace />} /> 
+                    <Route path="*" element= {<Navigate to="/" replace />} /> 
                 
             </Routes>
         </div>
---
+++ src/components/UserComponent.jsx
@@ -0,0 +1,105 @@
+import React, { useState, useEffect } from "react";
+
+function User () {
+
+    const [users, setUsers] = useState([]);
+    const [max , setMax] = useState(10);
+    const [min, setMin] = useState(0);
+
+    useEffect(() =>{
+        fetch(`/admin/getusers`)
+        .then(res => res.json())
+        .then(data => setUsers(data))
+    }, [])
+
+    let increaseCount = () => {
+        setMax(max + 10)
+        setMin(min + 10)
+    }
+    
+    let decreaseCount = () => {
+        setMax(max - 10)
+        setMin(min - 10)
+    }
+
+    let handleRoleChange = (id) => {
+        fetch(`admin/setuserrole/${id}`,        
+        {method: 'PUT', 
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        }
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+            let newUsers = [...users];
+            for (let i = 0; i < newUsers.length; i++){
+                if(newUsers[i].id === data.id){
+                    newUsers[i].role = data.role;
+                }  
+            }
+            setUsers(newUsers)
+        })
+    }
+
+    let handleDelete = (id) => {
+        fetch(`/admin/userdelete/${id}`, 
+        {method: 'DELETE', 
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        }
+    }).then(
+        setUsers(users.filter(prediction => prediction.id !== id))
+        );    
+    }
+
+    const userList = users.filter((item, i) => i < max && i >= min).map(user => {
+        return (
+            <tr key={user.id}>
+                <td>{user.name}</td>
+                <td>{user.username}</td>
+                <td>{user.role}</td>
+                <td>
+                    <div className="btn-group">
+                        <button className="btn btn-secondary" onClick={() => handleRoleChange(user.id)}>Change Role</button>
+                        <button className="btn btn-danger" onClick={() => handleDelete(user.id)}>Delete</button>
+                    </div>
+                </td>
+            </tr>
+        )
+    })
+
+    return (
+        <div className="container">
+            <h2 className="mb-5 mt-5">Current Users</h2>
+            <table className="table text-center">
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Username</th>
+                        <th>Role</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {userList}
+                </tbody>
+            </table>
+            <div className='row'>
+                    <div className='col-1' >
+                        <button className='btn btn-outline-dark rounded-circle' onClick={() => decreaseCount()} disabled={min <= 0}><i className="fa fa-arrow-left" aria-hidden="true"></i></button> 
+                    </div>
+                    <div className='col-10 text-center lead fs-4'>
+                        {min+1} ... {max}
+                    </div>
+                    <div className='col-1 float-end'>
+                        <button className='btn btn-outline-dark rounded-circle' onClick={() => increaseCount()}  disabled={max >= users.length}><i className="fa fa-arrow-right" aria-hidden="true"></i></button>
+                    </div>
+                </div>
+        </div>
+    )
+}
+
+export default User;
